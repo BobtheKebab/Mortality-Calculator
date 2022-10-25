d445e05bd64d466a5c27cf110a039aa76c12ff04
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2022-10-25 14:42:19 -0400

    selecting specific cols for table

--- calculator/models.py
+++ calculator/models.py
@@ -12,8 +12,8 @@ class DataParser:
         client = Socrata(data_url, app_token)
 
         payload = "year = '2019' AND sex = '" + pSex + "' AND race_ethnicity = '" + pEthnicity + "'"
-        #slct = "leading_cause, deaths, age_adjusted_death_rate"
-        results = client.get(data_set, limit=pLimit, where=payload)
+        cols = "leading_cause, deaths, age_adjusted_death_rate"
+        results = client.get(data_set, limit=pLimit, where=payload, select=cols)
         #, order="age_adjusted_death_rate DESC")
 
         results_df = pd.DataFrame.from_records(results)
