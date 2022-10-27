08557fa5bed0551f470e307380352efe156c9d1b
Bryan Lysander <bryanlysander13@gmail.com>
2022-10-27 19:18:47 -0400

    unit test for the drop down user input

--- calculator/tests.py
+++ calculator/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase #noqa
+from django.test import TestCase 
+from calculator.models import DataParser
+import pandas as pd
 
-# Create your tests here.
+class GetResultTestCase(TestCase):
+    def getResult(self):
+        sex = "Female"
+        ethnicity = "White"
+        limit = 5
+        #This will test when we have only 1 input which is sex
+        self.assertEqual(DataParser.getDeathCauses(sex, None, limit), None)
+        #This will test when we have only 1 input which is ethnicity
+        self.assertEqual(DataParser.getDeathCauses(None, ethnicity, limit), None)
+        #This will test when we have only 1 input which is limit
+        self.assertEqual(DataParser.getDeathCauses(sex, ethnicity, None), None)
