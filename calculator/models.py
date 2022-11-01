a42b0e6b0c7c71a54715e1835b878a8a57b07784
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2022-11-01 15:11:35 -0400

    Made visualize page and sent data to it

--- calculator/models.py
+++ calculator/models.py
@@ -2,24 +2,42 @@ from django.db import models #noqa
 import pandas as pd
 from sodapy import Socrata
 
+data_url = 'data.cityofnewyork.us'  # The Host Name for the API endpointy)
+data_set = 'jb7j-dtam'  # The data set at the API endpoint
+app_token = 'GI8oZAztXFWG3uda2SXGB1jGn'  # The app token created in the prior steps
+
 class DataParser:
 
-    @staticmethod
-    def getDeathCauses(pSex, pEthnicity, pLimit):
+    def __init__(self):
+        self.client = Socrata(data_url, app_token)
+
+
+
+    def getDeathCauses(self, pSex, pEthnicity, pLimit):
+
         if (pEthnicity is None):
             return None
         if (pSex is None):
             return None
         if (pLimit is None):
             return None
-        data_url = 'data.cityofnewyork.us'  # The Host Name for the API endpointy)
-        data_set = 'jb7j-dtam'  # The data set at the API endpoint
-        app_token = 'GI8oZAztXFWG3uda2SXGB1jGn'  # The app token created in the prior steps
-        client = Socrata(data_url, app_token)
 
         payload = "year = '2019' AND sex = '" + pSex + "' AND race_ethnicity = '" + pEthnicity + "'"
         cols = "leading_cause, deaths, age_adjusted_death_rate"
-        results = client.get(data_set, limit=pLimit, where=payload, select=cols)
+        results = self.client.get(data_set, limit=pLimit, where=payload, select=cols)
+        #, order="age_adjusted_death_rate DESC")
+
+        results_df = pd.DataFrame.from_records(results)
+        return results_df
+
+
+
+    def visualizeDeathCauses(self):
+        
+        payload = ""
+        #cols = "year, leading_cause, deaths, age_adjusted_death_rate"
+        cols = ""
+        results = self.client.get(data_set, limit=20, where=payload, select=cols)
         #, order="age_adjusted_death_rate DESC")
 
         results_df = pd.DataFrame.from_records(results)
