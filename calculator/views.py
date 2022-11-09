8c0690ce394353b92ca129d36bf79af09e268001
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2022-11-09 15:23:37 -0500

    colors for bars + hide controls + truncate long death causes

--- calculator/views.py
+++ calculator/views.py
@@ -11,17 +11,24 @@ def results(request):
 
     results_df = query_results
     results_df = results_df.astype({"age_adjusted_death_rate": float})
-    results_df = results_df.sort_values(by=['age_adjusted_death_rate'], ascending=True)
+    results_df = results_df.sort_values(by=['age_adjusted_death_rate'], ascending=False)
 
     fig = px.bar(results_df, x="age_adjusted_death_rate", y="leading_cause", orientation='h',
-    labels=dict(age_adjusted_death_rate="Age Adjusted Death Rate", leading_cause="Top 5 Causes of Death"))
+    labels=dict(age_adjusted_death_rate="Age Adjusted Death Rate", leading_cause="Top 5 Causes of Death"),
+    color="leading_cause", hover_data=["age_adjusted_death_rate"])
+
+    # Hide legend
+    fig.update_traces(showlegend=False)
+
+    # Hide controls for chart
+    config = dict({'displayModeBar': False})
 
     #placeholder labels
     #for idx in range(len(fig.data)):
         #fig.data[idx].y = ['1','2','3', '4', '5']
 
 
-    graph = fig.to_html(full_html=False, default_height=700, default_width=1200)
+    graph = fig.to_html(full_html=False, default_height=700, default_width=1200, config=config)
     query_results = graph
     context = {'query_results': query_results}
     return render(request, 'calculator/results.html', context)
