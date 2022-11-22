6970f5434022ed67526c818adcd3e35b509e7767
Ahbab Ashraf <ahbab.ashraf@gmail.com>
2022-11-21 21:17:29 -0500

    Updated citywide chart to show male and female separately + removed other from dropdown.
    Co-authored-by: Sara Abdelrazek 33522387+saraabdel@users.noreply.github.com

--- calculator/views.py
+++ calculator/views.py
@@ -41,11 +41,14 @@ def visualize(request):
     df = query_results
 
     fig = px.bar(df, x="leading_cause", y="age_adjusted_death_rate", color="race_ethnicity",
-    labels=dict(age_adjusted_death_rate="Age Adjusted Death Rate per 100,000", leading_cause="Cause of Death"),
-    hover_data=["age_adjusted_death_rate", "sex", "race_ethnicity", "year"])
-    
+            labels=dict(age_adjusted_death_rate="Age Adjusted Death Rate per 100,000", leading_cause="Cause of Death"),
+            hover_data=["age_adjusted_death_rate", "sex", "race_ethnicity", "year"], facet_col="sex",
+            animation_frame="year", animation_group="leading_cause")
+
     fig.update_layout(autosize=True, height=700)
     fig.update_layout(legend_title_text='Ethnicity')
+    fig['layout']['updatemenus'][0]['pad']=dict(r= 10, t= 150)
+    fig['layout']['sliders'][0]['pad']=dict(r= 10, t= 150)
 
     config = dict({'displayModeBar': False})
     graph = fig.to_html(full_html=False, config=config)
